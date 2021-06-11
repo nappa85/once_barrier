c127147afa87a5cf063b545ef37c9569626b9d35
Marco Napetti <nappa85@therocktrading.com>
2021-06-11 16:14:52 +0200

    still not working

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{cmp::Eq, collections::HashMap, future::Future, hash::Hash, sync::Arc, time::Duration};
+use std::{cmp::Eq, collections::HashMap, future::Future, hash::Hash, pin::Pin, sync::Arc, task::{Context, Poll}, time::Duration};
 
 use tokio::{sync::RwLock, time::sleep};
 
@@ -33,14 +33,15 @@ where T: Hash + Eq + Clone + Send + Sync + 'static {
     where
         // at the moment it's impossible to express the lifetime  of the future, until HRTB is stable
         // therefore we need to pass the owned value T instead of &T
-        Read: FnOnce(T) -> ReadFut,
-        Write: FnOnce(T) -> WriteFut,
-        ReadFut: Future<Output=Out>,
-        WriteFut: Future<Output=Out>,
+        Read: (FnOnce(&T) -> ReadFut) + Unpin,
+        Write: (FnOnce(&T) -> WriteFut) + Unpin,
+        ReadFut: Future<Output=Out> + Unpin,
+        WriteFut: Future<Output=Out> + Unpin,
     {
         // check if entry exists
         if self.can_read(&key).await {
-            return read_callback(key).await;
+            let w = Wrapper::new(&key, read_callback);
+            return w.await;
         }
 
         // entry still doesn't exists
@@ -49,7 +50,8 @@ where T: Hash + Eq + Clone + Send + Sync + 'static {
             // we are late, drop write lock to dequeue and retry
             drop(lock);
             if self.can_read(&key).await {
-                return read_callback(key).await;
+                let w = Wrapper::new(&key, read_callback);
+                return w.await;
             }
             else {
                 unreachable!();
@@ -63,7 +65,8 @@ where T: Hash + Eq + Clone + Send + Sync + 'static {
         drop(lock);
 
         // create the file
-        let res = write_callback(key.clone()).await;
+        let w = Wrapper::new(&key, write_callback);
+        let res = w.await;
         // delete the entry after given time
         let delay = self.delay;
         let inner = Arc::clone(&self.inner);
@@ -75,5 +78,63 @@ where T: Hash + Eq + Clone + Send + Sync + 'static {
         // free write lock and return
         drop(temp);
         res
+    }
+}
+
+struct Wrapper<'a, T, F, Fut>
+where
+    F: (FnOnce(&'a T) -> Fut) + Unpin,
+    Fut: Future + Unpin + 'a,
+{
+    param: &'a T,
+    inner: Option<F>,
+    temp: Option<Fut>,
+}
+
+impl<'a, T, F, Fut> Wrapper<'a, T, F, Fut>
+where
+    F: (FnOnce(&'a T) -> Fut) + Unpin,
+    Fut: Future + Unpin + 'a,
+{
+    fn new(param: &'a T, inner: F) -> Self {
+        Wrapper {
+            param,
+            inner: Some(inner),
+            temp: None,
+        }
+    }
+}
+
+impl<'a, T, F, Fut> Future for Wrapper<'a, T, F, Fut>
+where
+    F: (FnOnce(&'a T) -> Fut) + Unpin,
+    Fut: Future + Unpin + 'a,
+{
+    type Output = Fut::Output;
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.get_mut();
+        if let Some(f) = this.inner.take() {
+            this.temp = Some(f(this.param));
+        }
+        if let Some(f) = this.temp.as_mut() {
+            Pin::new(f).poll(cx)
+        }
+        else {
+            unreachable!()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+
+    use super::OnceBarrier;
+
+    #[tokio::test]
+    async fn it_works() {
+        let ob = OnceBarrier::new(Duration::from_secs(1));
+        println!("{}", ob.get(String::from("foo"), |foo| Box::pin(async move { println!("read {}", foo); 1_u8 }), |foo| Box::pin(async move { println!("write {}", foo); 2_u8 })).await);
+        println!("{}", ob.get(String::from("foo"), |foo| Box::pin(async move { println!("read {}", foo); 1_u8 }), |foo| Box::pin(async move { println!("write {}", foo); 2_u8 })).await);
     }
 }
